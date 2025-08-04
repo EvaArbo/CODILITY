5394424802f42ca2da95d8364dbc8f821909b114
EvaArbo <evelynearbogusti@gmail.com>
2025-08-05 00:41:07 +0300

    added while loop

---
+++ PYTHON/squared.py
@@ -0,0 +1,7 @@
+def square_integers(int_list):
+    return [x**2 for x in int_list]
+
+# Example usage
+print(square_integers([1, 2, 3, 4, 5]))
+# Output: [1, 4, 9, 16, 25]
+# This function takes a list of integers and returns a new list with each integer squared.
---
+++ PYTHON/while.py
@@ -0,0 +1,8 @@
+def happy_new_year():
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!🎉")
+
+happy_new_year()
